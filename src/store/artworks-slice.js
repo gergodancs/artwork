1148efb69d9af1a95926ed703178154da51625e2
gergodancs <dancs.gergely@gmail.com>
2022-06-21 15:46:44 +0200

    Add details page

--- src/store/artworks-slice.js
+++ src/store/artworks-slice.js
@@ -3,7 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   loading: false,
   results: [],
+
   error: "",
+  page: 1,
+  numberOfArts: 25,
 };
 
 const artworkSlice = createSlice({
@@ -13,17 +16,33 @@ const artworkSlice = createSlice({
     addData(state, action) {
       state.results = action.payload.data;
     },
+    incerementPage(state) {
+      console.log("hello");
+      state.page = state.page + 1;
+    },
+    decrementPage(state) {
+      if (state.page === 1) {
+        return;
+      } else {
+        state.page = state.page - 1;
+      }
+    },
+    selectNumOfArts(state, action) {
+      state.numberOfArts = action.payload;
+    },
+    addFavourites(state, action) {},
+    replaceFavourite(state, action) {},
   },
 });
 
-export const fetchArtworks = () => {
+export const fetchArtworks = (searchText, page, numofArts) => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        `https://api.artic.edu/api/v1/artworks/search?q=cats&page=1&limit=10&fields=id,title,image_id,thumbnail`
+        `https://api.artic.edu/api/v1/artworks/search?q=${searchText}&page=${page}&limit=${numofArts}&fields=id,title,image_id,thumbnail`
       );
       if (!response.ok) {
-        console.log("not ok");
+        throw new Error(response.message);
       }
       const data = await response.json();
       return data;
@@ -32,7 +51,7 @@ export const fetchArtworks = () => {
       const results = await fetchData();
       dispatch(artworkSlice.actions.addData(results));
     } catch (error) {
-      console.log("cath error");
+      console.log(error);
     }
   };
 };
